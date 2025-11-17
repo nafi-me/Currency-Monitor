f76441108c14d91f888ba29bfaf45b4aade1fb05
MD SHAHRIAR RASHID <143961339+nafi-me@users.noreply.github.com>
2025-11-18 02:12:12 +0300

    Create CurrencyList.java

---
+++ CurrencyList.java
@@ -0,0 +1,9 @@
+package com.livefx;
+
+public class CurrencyList {
+    public static final String[] ALL = {
+            "USD","EUR","GBP","INR","JPY","CAD","AUD","AED","SAR","KWD",
+            "BDT","RUB","CNY","HKD","SGD","NZD","PKR","TRY","ZAR","CHF",
+            "SEK","NOK","DKK","PLN","MXN","THB","MYR","IDR","PHP","EGP"
+    };
+}
